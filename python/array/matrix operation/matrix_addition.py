39a5779fe6c223edddc5dc776a2571e71b4f9ff2
notshahzad <shahzadakhtar182005@gmail.com>
2021-08-01 12:22:34 +0530

    matrix addition

--- python/array/matrix operation/matrix_addition.py
+++ python/array/matrix operation/matrix_addition.py
@@ -1,10 +1,10 @@
 m_a = [[3,4],[6,7]]
 m_b = [[6,1],[-1,-2]]
-def squarematrixadd(a,b):
+def matrixadd(a,b):
     length = len(a)
     c = [[None]*length,[None]*length]
     for i in range(length):
         for j in range(length):
             c[i][j] = a[i][j]+b[i][j]
     return c
-print(squarematrixadd(m_a,m_b))
+print(matrixadd(m_a,m_b))

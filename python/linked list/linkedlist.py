78842c8c37ae37cc684bc8b41e476e29004509e7
notshahzad <shahzadakhtar182005@gmail.com>
2021-08-04 16:22:01 +0530

    linked list

--- python/linked list/linkedlist.py
+++ python/linked list/linkedlist.py
@@ -4,26 +4,24 @@ class Node():
     def __init__(self,element):
         self.element = element
         self.pointer = None
-def createNode(element):
+def CreateLinkedList(element):
     global first, last
     if first == None:
-        last = first = Node(element)
+        first = last = Node(element)
     else:
         new = Node(element)
         last.pointer = new
         last = new
-    
 def PrintLinkedList():
     i = first
     while 1:
         print(i.element)
-        if i == last:
+        if i==last:
             break
         i = i.pointer
 while 1:
-    print('press i to insert and p to print')
-    choice = input().lower()
-    if choice == 'i':
-        createNode(input())
-    elif choice == 'p':
+    choice = input('press i for insertion and p for print.\n')
+    if choice.lower() == 'i':
+        CreateLinkedList(input('enter element :'))
+    elif choice.lower() == 'p':
         PrintLinkedList()

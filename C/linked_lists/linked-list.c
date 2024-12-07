d6991641dfd7fe46a1abef46d39d8ad95307817d
shahzad <shahzadakhtar182005@gmail.com>
2024-12-07 11:58:56 +0530

    linkedlist: refactor shit coed

--- C/linked_lists/linked-list.c
+++ C/linked_lists/linked-list.c
@@ -1,64 +1,54 @@
+#include <stdint.h>
 #include <stdio.h>
-struct node
-{
-    int data;
-    struct node *pointer;
-};
-void PrintLinkedList(struct node *head)
-{
-    struct node *temp;
-    temp = head;
-    while (temp != NULL)
-    {
-        printf("%d - ", temp->data);
-        temp = temp->pointer;
-    }
-    printf("\n");
+#include <stdlib.h>
+typedef struct node {
+  int val;
+  struct node *next;
+} node;
+void print_linked_list(node *head) {
+  node *temp;
+  temp = head;
+  while (temp != NULL) {
+    printf("%d - ", temp->val);
+    temp = temp->next;
+  }
+  printf("\n");
 }
-struct node *CreateNode(int d)
-{
-    struct node *n;
-    // int d;
-    // scanf("%d", &d);
-    n = malloc(sizeof(struct node));
-    n->data = d;
-    n->pointer = NULL;
-    return n;
+node *create_node(int data) {
+  node *n;
+  n = malloc(sizeof(node));
+  n->val = data;
+  n->next = NULL;
+  return n;
+}
+// note(shahzad): should allocate statically but idk
+node *node_new(int data) {
+  node *n = malloc(sizeof(*n));
+  n->val = data;
+  n->next = NULL;
+  return n;
+}
+void node_link(node *n1, node *n2) { n1->next = n2; }
+
+typedef struct ll {
+  node *head;
+  node *tail;
+} ll;
+void ll_append(ll *self, int data) {
+  node *n = node_new(data);
+  if (self->head == NULL) {
+    self->head = self->tail = n;
+  } else {
+    node_link(self->tail, n);
+    self->tail = n;
+  }
 }
 
-int main(void)
-{
-    struct node *head;
-    struct node *new;
-    struct node *tail;
-    head = CreateNode(NULL);
-    int choice = 1;
-    while (1)
-    {
-        scanf("%d", &choice);
-        if (choice == 1)
-        {
-            int data;
-            scanf("%d", &data);
-            if (head->data == NULL)
-            {
-                head = CreateNode(data);
-                tail = head;
-            }
-            else
-            {
-                new = CreateNode(data);
-                tail->pointer = new;
-                tail = new;
-            }
-        }
-        else if (choice == 2)
-        {
-            PrintLinkedList(head);
-        }
-        else
-        {
-            break;
-        }
-    }
+int main(void) {
+  ll ll1 = {0};
+  ll_append(&ll1, 1);
+  ll_append(&ll1, 2);
+  ll_append(&ll1, 3);
+  ll_append(&ll1, 4);
+  ll_append(&ll1, 5);
 }
